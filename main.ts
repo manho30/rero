4bb54691cb396738a1427543422b358bf8a7ec3f
manho <86037446+manho30@users.noreply.github.com>
2022-06-16 22:23:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-let strip: neopixel.Strip = null
-basic.forever(function () {
-    strip = neopixel.create(DigitalPin.P1, 2, NeoPixelMode.RGB)
-    strip.setBrightness(255)
-    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    strip.show()
-    strip.clear()
-    basic.pause(500)
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
 })
