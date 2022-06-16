798c3e0beefa29c5ca995764e712e47a6c1bf412
manho <86037446+manho30@users.noreply.github.com>
2022-06-16 22:17:08 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let strip: neopixel.Strip = null
+basic.forever(function () {
+    strip = neopixel.create(DigitalPin.P1, 2, NeoPixelMode.RGB)
+    strip.setBrightness(255)
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    strip.clear()
+    basic.pause(500)
+})
